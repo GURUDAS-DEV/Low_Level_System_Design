ea9e5d0516d9cbb4e1df14116498de5b84707b1c
Gurudas Bhardwaj <gursad5@gmail.com>
2026-03-04 10:00:00 +0530

    Minor Updates

--- DesignPattern/Creational/AbstractFactoryExercise/Program.cs
+++ DesignPattern/Creational/AbstractFactoryExercise/Program.cs
@@ -1,1 +1,19 @@
-﻿ 
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        IGUIFactory factory = new WinFactory();
+        IButton button = factory.CreateButton();
+        ITextBox textBox = factory.CreateTextBox();
+
+        button.Render();
+        textBox.Render();
+
+        factory = new MacFactory();
+        button = factory.CreateButton();
+        textBox = factory.CreateTextBox();
+
+        button.Render();
+        textBox.Render();
+    }
+}
