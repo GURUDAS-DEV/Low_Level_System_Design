3790441a190543b6b6038701c7b59ef2d3e9b8a4
Gurudas Bhardwaj <gursad5@gmail.com>
2026-03-08 10:54:50 +0530

    Update AssemblyInfo with new informational version and cache input

--- Enums/obj/Debug/net10.0/Enums.AssemblyInfo.cs
+++ Enums/obj/Debug/net10.0/Enums.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Enums")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62c0f7c69d2ca4965eeb7d2da9c41b914c1d166a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3d029d1a49d5630e4bab418338fdbeb98c08d1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Enums")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Enums")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
