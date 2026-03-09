e0d40be3b76d152fbb94f1dbebbeb56443890ba8
Gurudas Bhardwaj <gursad5@gmail.com>
2026-03-09 23:32:11 +0530

    Added feature

--- Enums/obj/Debug/net10.0/Enums.AssemblyInfo.cs
+++ Enums/obj/Debug/net10.0/Enums.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Enums")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3d029d1a49d5630e4bab418338fdbeb98c08d1d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7703196d2e4b90162f96cf112e0e842db54e225f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Enums")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Enums")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
